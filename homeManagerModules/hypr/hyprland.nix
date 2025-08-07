29358688bb6c54dd7e10827f2f923d78fc5d39d0
max <70414866+viamaximus@users.noreply.github.com>
2025-08-06 21:21:15 -0400

    updated hyprland.nix to move active window's workspace

--- homeManagerModules/hypr/hyprland.nix
+++ homeManagerModules/hypr/hyprland.nix
@@ -105,8 +105,18 @@ in {
 	  "$mainMod, 6, workspace, 6"
 	  "$mainMod, 7, workspace, 7"
 	  "$mainMod, 8, workspace, 8"
-	  "$mainMod, 9, workspace, 9"
-	  #move active window to workspace with mainMod + shift + #
+      "$mainMod, 9, workspace, 9"
+      #move active window to workspace
+      "$mainMod SHIFT, 1, movetoworkspace, 1"
+      "$mainMod SHIFT, 2, movetoworkspace, 2"
+      "$mainMod SHIFT, 3, movetoworkspace, 3"
+      "$mainMod SHIFT, 4, movetoworkspace, 4"
+      "$mainMod SHIFT, 5, movetoworkspace, 5"
+      "$mainMod SHIFT, 6, movetoworkspace, 6"
+      "$mainMod SHIFT, 7, movetoworkspace, 7"
+      "$mainMod SHIFT, 8, movetoworkspace, 8"
+      "$mainMod SHIFT, 9, movetoworkspace, 9"
+
 
           ",XF86MonBrightnessUp, exec, brightnessctl set +10%"
           ",XF86MonBrightnessDown, exec, brightnessctl set 10%-"
