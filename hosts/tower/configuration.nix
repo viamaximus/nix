32ad95f4d285dc9903dbf879e91c9be73672847e
viamaximus <70414866+viamaximus@users.noreply.github.com>
2026-01-02 02:30:31 -0500

    add home tower host

---
+++ hosts/tower/configuration.nix
@@ -0,0 +1,180 @@
+{ config, pkgs, inputs, ... }:
+let
+  wallpaperConfig = import ./current-wallpaper.nix;
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.home-manager
+      inputs.stylix.nixosModules.stylix
+      ../../nixosModules/automount.nix
+    ];
+
+  stylix = {
+    enable = true;
+    image = wallpaperConfig.currentWallpaper;
+    polarity = "dark";
+    cursor = {
+      name = "Catppuccin-Macchiato-Mauve";
+      package = pkgs.catppuccin-cursors.macchiatoMauve;
+      size = 24;
+    };
+  };
+
+
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      max = import ./home.nix;
+    };
+    useGlobalPkgs = true;
+    backupFileExtension = "backup";
+  };
+
+
+  # Bootloader.
+  boot.loader = {
+    grub = {
+      enable = true;
+      efiSupport = true;
+      devices = ["nodev"];
+    };
+  };
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "tower"; # Define your hostname.
+
+  networking = {
+    networkmanager.enable = true;
+	useNetworkd = false;
+	};
+  systemd.network.wait-online.enable = false;
+  systemd.services.NetworkManager-wait-online.enable = false;
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+  
+  hardware.graphics = {
+    enable = true;
+  };
+  
+  hardware.nvidia = {
+	open = false;
+	modesetting.enable = true;
+	powerManagement.enable = true;
+	# powerManagement.finegrained = false;
+  };
+  
+  boot.blacklistedKernelModules = [ "nouveau" ];
+
+  fileSystems."/mnt/games" = {
+	device = "/dev/disk/by-uuid/4808c298-cd75-4cbd-bdef-71c063463b2e";
+	fsType = "ext4";
+	options = [
+	  "noatime"
+	  "discard"
+	];
+  };
+  systemd.tmpfiles.rules = [
+    "d /mnt/games 0755 max max -"
+  ];
+
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  programs.fish.enable = true;
+  users.users.max = {
+    shell = pkgs.fish;
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+
+  programs.xwayland.enable = true;
+  programs.hyprland.enable = true;
+  programs.hyprland.package = inputs.hyprland.packages."${pkgs.system}".hyprland;
+  programs.hyprlock.enable = true;
+  security.pam.services.hyprlock = {};
+
+  services.tailscale.enable = true;
+
+  ###gaming support
+  programs.gamemode.enable = true;
+  programs.steam.enable = true;
+
+  services.displayManager.ly.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  users.users.max = {
+    isNormalUser = true;
+    description = "max";
+    group = "max";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+  users.groups.max = {};
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  programs.firefox.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    neovim
+    kitty
+	git
+  ];
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.fira-code
+    nerd-fonts.droid-sans-mono
+    nerd-fonts.noto
+    nerd-fonts.hack
+    nerd-fonts.ubuntu
+  ];
+
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
---
+++ hosts/tower/home.nix
@@ -0,0 +1,71 @@
+{
+  config,
+  pkgs,
+  lib,
+  inputs,
+  ...
+}:
+{
+  imports = [
+    ../../homeManagerModules
+	../../homeManagerModules/gaming.nix
+	../../homeManagerModules/programs/3dprint.nix
+  ];
+
+  home.username = "max";
+  home.homeDirectory = "/home/max";
+
+  home.packages = with pkgs; [
+    hello
+  ];
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  programs.fish = {
+    enable = true;
+    shellInit = ''
+      fish_add_path $HOME/.npm-global/bin
+      fish_add_path $HOME/.local/bin
+    '';
+  };
+
+  home.file = {
+    #put .rc files here
+  };
+
+  wayland.windowManager.hyprland = {
+	enable = true;
+	extraConfig = ''
+      env = XDG_SESSION_TYPE,wayland
+	  env = NIXOS_OZONE_WL,1
+	  env = QT_QPA_PLATFORM,wayland
+	  env = MOZ_ENABLE_WAYLAND,1
+      env = WLR_NO_HARDWARE_CURSORS,1
+
+	  # monitor def
+	  # left: 1080@60Hz
+	  monitor = DP-1,1920x1080@60,0x0,1
+
+      # center: 1440p@179.88Hz
+      monitor = DP-3,2560x1440@179.88,1920x-180,1
+
+	  # right: 1080@60Hz (portrait right)
+      monitor = DP-2,1920x1080@60,4480x-420,1,transform,1
+
+	  workspace = 1, monitor:DP-1
+	  workspace = 2, monitor:DP-3
+	  workspace = 3, monitor:DP-2
+
+	  # make games always open on center monitor
+	  windowrule = monitor DP-3, class:^(steam_app_[0-9]+)$
+	  windowrule = monitor DP-3, class:^(gamescope)$
+	  windowrule = workspace 2, class:^(steam_app_[0-9]+)$
+	  
+	'';
+  };
+
+  home.stateVersion = "25.05";
+  programs.home-manager.enable = true;
+}
